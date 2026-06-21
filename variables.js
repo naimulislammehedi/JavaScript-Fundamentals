97f81fd372971ed6a6476b4c8a23047ebf1a954d
Naimul Islam Mehedi <mehedi.interface@gmail.com>
2026-06-21 08:24:52 +0600

    JavaScript Variables

---
+++ variables.js
@@ -0,0 +1,49 @@
+// variables are used to store data in memory 
+// use let when the value may change 
+let name = "Rahim"; 
+console.log(name); 
+
+// without variables 
+console.log("Mehedi"); 
+console.log("Mehedi"); 
+console.log("Mehedi"); 
+console.log("Mehedi"); 
+
+// with variables 
+console.log(name); 
+console.log(name); 
+console.log(name); 
+console.log(name); 
+console.log(name); 
+console.log(name); 
+
+/*
+- var 
+- let 
+- const 
+*/
+
+// const: use const when the value should not change 
+const country = "Bangladesh"; 
+console.log(country); 
+
+// country = "China"; 
+console.log(country)
+
+// var: old way 
+var city = "Dhaka"; 
+console.log(city); 
+
+// Naming rules
+let firstName; 
+let user_1; 
+let $money; 
+
+// real life example 
+const studentName = "Mehedi"; 
+let age = 25; 
+let course = "JavaScript"; 
+
+console.log(studentName); 
+console.log(age); 
+console.log(course); 
