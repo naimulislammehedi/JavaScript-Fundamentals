39e8bf6618886510f13234b8f11ba203e2045a6e
Naimul Islam Mehedi <mehedi.interface@gmail.com>
2026-06-25 21:27:40 +0600

    test

---
+++ test.js
@@ -0,0 +1,19 @@
+// A function is a reusable block of code that performs a specific task 
+
+/*
+Think of it like a blender: 
+- Input -> Ingredients 
+- Process -> Blending 
+- Output -> Smoothie 
+
+In JavaScript 
+- Input -> Parameters 
+- Process -> Function body 
+- Output -> Return value
+*/
+
+function greet() {
+    console.log("Hello, World"); 
+}
+
+greet(); 
